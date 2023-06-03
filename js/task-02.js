3fa743ddfb2b816afd955f12fe025fbc199ec7ae
SyvykNatalia <nazarsyvyk123@gmail.com>
2023-06-03 22:38:19 +0300

    task2

---
+++ js/task-02.js
@@ -0,0 +1,16 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const ingredientsContainer = document.querySelector('#ingredients')
+const elements = ingredients.forEach(ingredient => {
+  const liElement = document.createElement('li');
+  liElement.textContent = ingredient;
+  liElement.classList.add('item')
+})
+console.log(elements)
+ingredientsContainer.append(...elements);
---
+++ js/task-03.js
@@ -0,0 +1,18 @@
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  }]
+
+const galleryContainer = document.querySelector('.gallery');
+const imgElement = images.map(el =>  `<li class ='list-gallery'><img class = 'img-gallery' src = '${el.url}' alt = '${el.alt}' width = 300 </li>`).join('');
+galleryContainer.insertAdjacentHTML('afterbegin', imgElement);
+console.log(imgElement);
